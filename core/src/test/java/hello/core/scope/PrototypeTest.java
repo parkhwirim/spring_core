870cf2c0cb9c586e7e93e21f0fefc922849e139c
parkhwirim <hwirim7907@gmail.com>
2023-12-12 22:45:34 +0900

    프로토타입 스코프, 싱글톤 빈과 함께 사용 시 문제점

---
+++ core/src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,40 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+
+        System.out.println(prototypeBean1);
+        System.out.println(prototypeBean2);
+
+        assertNotSame(prototypeBean1, prototypeBean2);
+
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("destroy");
+        }
+    }
+}
---
+++ core/src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,40 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SingletonTest {
+
+    @Test
+    void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+
+        System.out.println(singletonBean1);
+        System.out.println(singletonBean2);
+
+        assertEquals(singletonBean1, singletonBean2);
+
+        ac.close();
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("destroy");
+        }
+    }
+}
